f1ce3e51b12ff9bdc046e63e15401ef401b11f06
evgenia-taipova <94874062+evgenia-taipova@users.noreply.github.com>
2023-12-14 23:29:18 +0100

    Update GoalsEditPageView.dart

--- lib/Views/GoalsEditPageView.dart
+++ lib/Views/GoalsEditPageView.dart
@@ -1,10 +1,24 @@
+// File: GoalsEditPageView.dart
+// Author: Taipova Evgeniya (xtaipo00)
+// Description: This file contains the implementation of the GoalsEditPageView class,
+// which allows users to edit their financial goals in the finance application.
+
 import 'package:flutter/material.dart';
 import 'package:itu_dev/Views/GoalsPageView.dart';
 import 'package:itu_dev/Views/GoalsEditDeletePage.dart';
 import 'package:itu_dev/Controllers/GoalsPageController.dart';
+import 'package:intl/intl.dart';
 
 class GoalsEditPageView extends StatefulWidget {
-  const GoalsEditPageView({super.key,required this.id, required this.name,required this.goalAmount, required this.amount, required this.date});
+
+  const GoalsEditPageView({
+    super.key,
+    required this.id,
+    required this.name,
+    required this.goalAmount,
+    required this.amount,
+    required this.date,
+  });
 
   final id;
   final name;
@@ -16,17 +30,21 @@ class GoalsEditPageView extends StatefulWidget {
   State<GoalsEditPageView> createState() => _GoalsEditPageViewState();
 }
 
-class _GoalsEditPageViewState extends State<GoalsEditPageView>{
+class _GoalsEditPageViewState extends State<GoalsEditPageView> {
   final GoalsPageController _controller = GoalsPageController();
-
+  late TextEditingController dateController;
 
   @override
-  Widget build(BuildContext context){
-    String newName=widget.name;
-    String newGoalAmount=widget.goalAmount;
-    String newAmount=widget.amount;
-    String newDate=widget.date;
+  void initState() {
+    super.initState();
+    dateController = TextEditingController(text: widget.date);
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    String newName = widget.name;
+    String newGoalAmount = widget.goalAmount;
+    String newAmount = widget.amount;
 
     return Scaffold(
       appBar: AppBar(
@@ -34,13 +52,16 @@ class _GoalsEditPageViewState extends State<GoalsEditPageView>{
         title: const Text("Edit", style: TextStyle(fontSize: 28, color: Colors.white)),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed:  (){_controller.gotoPage(GoalsEditDeletePage(id: widget.id, name: widget.name, goalAmount: widget.goalAmount,amount: widget.amount, date: widget.date), context);},
+          onPressed:  (){
+            // Navigate back to GoalsEditDeletePage when the back button is pressed.
+            _controller.gotoPage(GoalsEditDeletePage(id: widget.id, name: widget.name, goalAmount: widget.goalAmount,amount: widget.amount, date: widget.date), context);},
         ),
         actions: <Widget>[
           TextButton(
               child: const Text('Save', style: TextStyle(color: Colors.white, fontSize: 20)),
               onPressed: (){
-                _controller.edit(widget.id, newName, newGoalAmount, newAmount,  newDate);
+                // Save the edited goal and navigate back to GoalsPageView.
+                _controller.edit(widget.id, newName, newGoalAmount, newAmount, dateController.text);
                 _controller.gotoPage(const GoalsPageView(title: "My Goals"), context);
               }
           )
@@ -49,6 +70,7 @@ class _GoalsEditPageViewState extends State<GoalsEditPageView>{
       body: SingleChildScrollView(
         child: Column(
           children: [
+            // Text field for editing the name of the goal.
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
@@ -69,6 +91,7 @@ class _GoalsEditPageViewState extends State<GoalsEditPageView>{
                 ),
               ),
             ),
+            // Text field for editing the goal amount.
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
@@ -90,6 +113,7 @@ class _GoalsEditPageViewState extends State<GoalsEditPageView>{
                 ),
               ),
             ),
+            // Text field for editing the collected amount.
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
@@ -111,16 +135,28 @@ class _GoalsEditPageViewState extends State<GoalsEditPageView>{
                 ),
               ),
             ),
+            // Text field for editing the desired date.
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
                 style: const TextStyle(color: Colors.white),
-                controller: TextEditingController(text: widget.date),
-                onChanged: (text){newDate = text;} ,
+                controller: dateController,
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: widget.date.isNotEmpty ? DateFormat('dd-MM-yyyy').parse(widget.date) : DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2025),
+                  );
+                  if (pickedDate != null) {
+                    String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
+                    dateController.text = formattedDate;
+                  }
+                },
                 decoration: const InputDecoration(
                   labelStyle: TextStyle(color: Color.fromARGB(100, 255, 255, 255)),
                   border: OutlineInputBorder(),
-
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Color.fromARGB(100, 255, 255, 255)),
                   ),
