aae2b9d77034f91db9bbe2084c4c27c8a4812605
evgenia-taipova <94874062+evgenia-taipova@users.noreply.github.com>
2023-12-14 23:29:42 +0100

    Update GoalsPageView.dart

--- lib/Views/GoalsPageView.dart
+++ lib/Views/GoalsPageView.dart
@@ -1,3 +1,9 @@
+// File: GoalsPageView.dart
+// Author: Taipova Evgeniya (xtaipo00)
+// Description: This file contains the implementation of the GoalsPageView class,
+// which is responsible for displaying and managing goals in the finance application.
+
+
 import 'package:flutter/material.dart';
 import 'package:itu_dev/Views/GoalsAddPageView.dart';
 import 'package:itu_dev/Views/BottomNavigationBarWidgetView.dart';
@@ -26,6 +32,7 @@ class _GoalsPageViewState extends State<GoalsPageView>{
         toolbarHeight: 120,
         title: Text(widget.title, style: const TextStyle(fontSize: 28, color: Colors.white)),
         actions: <Widget>[
+          // Add button to navigate to the GoalsAddPageView.
           IconButton(
               icon: const Icon(Icons.add, color: Colors.white),
               iconSize: 35,
@@ -42,15 +49,20 @@ class _GoalsPageViewState extends State<GoalsPageView>{
           child: SizedBox(
             width: double.infinity,
             child: FutureBuilder<Column?>(
+              // Fetch and display goals using the GoalsPageController.
               future: _controller.drawBubbleGoal(context, 255),
               builder: (context, AsyncSnapshot<Column?> snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
+                  // Show loading indicator while fetching data.
                   return const CircularProgressIndicator();
                 } else if (snapshot.hasError) {
+                  // Show error message if there's an error.
                   return Text('Error: ${snapshot.error}');
                 } else if (snapshot.hasData && snapshot.data != null) {
+                  // Display the list of goals when data is available.
                   return snapshot.data!;
                 } else {
+                  // Display a message when no data is available.
                   return const Center(
                     child: Text('No data available'),
                   );
@@ -60,7 +72,7 @@ class _GoalsPageViewState extends State<GoalsPageView>{
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBarWidgetView(),
+      bottomNavigationBar: BottomNavigationBarWidgetView(), // Display the bottom navigation bar.
     );
   }
 }
